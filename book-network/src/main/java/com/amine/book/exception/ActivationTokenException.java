88c6a6df2f7738484046e64177aa276f3b670fad
Amine Hmidani filali <hmidanifilaliamine@gmail.com>
2024-11-17 18:16:33 +0100

    (backend) book social network

---
+++ book-network/src/main/java/com/amine/book/exception/ActivationTokenException.java
@@ -0,0 +1,7 @@
+package com.amine.book.exception;
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
